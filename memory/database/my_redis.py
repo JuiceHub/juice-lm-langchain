7a8cc44d170e68850e0607517fe07437aa752057
JuiceHub <183328242@qq.com>
2024-07-02 17:27:16 +0800

    fix: new agent of glm4 and new tutorials

--- memory/database/my_redis.py
+++
@@ -1,3 +0,0 @@
-import redis
-
-my_redis = redis.Redis(host="192.168.1.119", port=6380, decode_responses=True)
--- tutorials/basic/1. simple_llm_application.py
+++ tutorials/basic/1. simple_llm_application.py
@@ -3,21 +3,20 @@ import os
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 import os
-from langchain_google_vertexai import ChatVertexAI
+from langchain_google_genai import ChatGoogleGenerativeAI
+# from model.language_models.llama.llama3 import Llama3
 
+import torch
 from langchain_core.messages import HumanMessage, SystemMessage
+from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
 
 os.environ["LANGCHAIN_TRACING_V2"] = "true"
 os.environ["LANGCHAIN_API_KEY"] = "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"
 
 os.environ["GOOGLE_API_KEY"] = "AIzaSyAr_chv_fdwm2uJWBQ8tFH2KiKei6wmSxI"
+model = ChatGoogleGenerativeAI(model="gemini-pro")
 
-model = ChatVertexAI(model="gemini-pro")
-
-messages = [
-    SystemMessage(content="Translate the following from English into Italian"),
-    HumanMessage(content="hi!"),
-]
+# model = Llama3()
 
 system_template = "Translate the following into {language}:"
 
@@ -25,12 +24,10 @@ prompt_template = ChatPromptTemplate.from_messages(
     [("system", system_template), ("user", "{text}")]
 )
 
-model.invoke(messages)
-
 parser = StrOutputParser()
-
-result = model.invoke(messages)
 
 chain = prompt_template | model | parser
 
-chain.invoke({"language": "italian", "text": "hi"})
+result = chain.invoke({"language": "italian", "text": "hi"})
+
+print(result)
